239436df18fdd8bf841df33f9e3f89edf41a2c4e
Hillary Brito <hillarybrito@hillarys-air.myfiosgateway.com>
2023-08-21 02:34:54 -0400

    created navlinks for home page and profile

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import NavBar from "./components/NavBar/NavBar";
+
 function App() {
   return (
     <div className="App">
      <Router>
+      <NavBar/>
       <Routes>
         <Route/>
       </Routes>
