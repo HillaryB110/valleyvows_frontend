be9a61c1af58f39d40f1c007433e67052ad3b816
Hillary Brito <hillarybrito@hillarys-air.myfiosgateway.com>
2023-08-23 19:07:12 -0400

    created api calls for usergifts and gifts

--- src/components/API/API.js
+++ src/components/API/API.js
@@ -96,6 +96,99 @@ async function deleteGiftSentAPI(id) {
     console.log(error);
   }
 }
+
+async function getAllUsersGiftsAPI() {
+  try {
+    let result = await Axios.get("/user-gifts");
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function getUserGiftAPI(id) {
+  try {
+    let result = await Axios.get(`/user-gifts/${id}`);
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function createUserGiftAPI(data) {
+  try {
+    let result = await Axios.post("/user-gifts", data);
+    console.log(result);
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function updateUserGiftAPI(id, data) {
+  try {
+    let result = await Axios.put(`/user-gifts/${id}`, data);
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function deleteUserGiftAPI(id) {
+  try {
+    let result = await Axios.delete(`/user-gifts/${id}`);
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+
+async function getAllGiftsAPI() {
+  try {
+    let result = await Axios.get("/gifts");
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function getGiftAPI(id) {
+  try {
+    let result = await Axios.get(`/gifts/${id}`);
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function createGiftAPI(data) {
+  try {
+    let result = await Axios.post("/gifts", data);
+    console.log(result);
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function updateGiftAPI(id, data) {
+  try {
+    let result = await Axios.put(`/gifts/${id}`, data);
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function deleteGiftAPI(id) {
+  try {
+    let result = await Axios.delete(`/gifts/${id}`);
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+}
  
 
 export {
@@ -110,4 +203,14 @@ export {
   getGiftSentAPI,
   updateGiftSentAPI,
   deleteGiftSentAPI,
+  getAllUsersGiftsAPI,
+  getUserGiftAPI,
+  createUserGiftAPI,
+  updateUserGiftAPI,
+  deleteUserGiftAPI,
+  getAllGiftsAPI,
+  getGiftAPI,
+  createGiftAPI,
+  updateGiftAPI,
+  deleteGiftAPI
 };
