67d69d04e5b9fe70d1335ced3b77e9aa7d1493fb
Hillary Brito <hillarybrito@hillarys-air.myfiosgateway.com>
2023-08-24 19:29:22 -0400

    added in gift assets and made image previews for avatars and best gift

--- src/components/CreateUserProfile/CreateUserProfile.js
+++ src/components/CreateUserProfile/CreateUserProfile.js
@@ -1,9 +1,12 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./CreateUserProfile.css";
 import { displayUserProfileAPI } from "../API/userProfileAPI";
+import { getAllGiftsAPI } from "../API/giftsAPI";
 import { useNavigate } from "react-router-dom";
 import { getImagesForUserAvatars } from "../data/Images";
+import { getImagesForGifts } from "../data/GiftImages";
 import SeasonBirthday from "../SeasonBirthday/SeasonBirthday";
+
 
 const CreateUserProfile = () => {
   let navigate = useNavigate();
@@ -13,8 +16,23 @@ const CreateUserProfile = () => {
   const [selectedSkill, setSelectedSkill] = useState("");
   const [bioShort, setBioShort] = useState("");
   const [fullProfile, setFullProfile] = useState("");
+  const [gifts, setGifts] = useState([]);
   const [bestGift, setBestGift] = useState("");
+  const [bestGiftImage, setBestGiftImage] = useState("");
   const [userAvatar, setUserAvatar] = useState("");
+
+ useEffect(() => {
+   async function fetchGifts() {
+     try {
+       const response = await getAllGiftsAPI();
+       setGifts(response.data); // Set the fetched gift data
+       console.log(gifts)
+     } catch (error) {
+       console.error("Error fetching gifts:", error);
+     }
+   }
+   fetchGifts();
+ }, []);
 
   const skills = [
     "Mining",
@@ -39,6 +57,8 @@ const CreateUserProfile = () => {
     "Non-Binary5.png",
     "Non-Binary6.png",
   ];
+
+  
 
   const handleFirstNameChange = (event) => {
     setFirstname(event.target.value);
@@ -66,7 +86,18 @@ const CreateUserProfile = () => {
   };
 
   const handleBestGiftChange = (event) => {
-    setBestGift(event.target.value);
+    const selectedGiftName = event.target.value;
+    setBestGift(selectedGiftName);
+
+    // Find the selected gift and get its image URL
+    const selectedGift = gifts.find((gift) => gift.gift_name === selectedGiftName);
+    if (selectedGift) {
+      const giftImage = getImagesForGifts(selectedGift.id); 
+      setBestGiftImage(giftImage);
+    } else {
+      setBestGiftImage(""); 
+    }
+    
   };
   const handleUserAvatarChange = (event) => {
     const selectedAvatar = event.target.value;
@@ -176,9 +207,32 @@ const CreateUserProfile = () => {
                     cols={50} // You can adjust the number of columns as needed
                     placeholder="Enter your full profile..."
                   />
-                  
                 </label>
               </label>
+              <label className="form-text">
+                Best Gift:
+                <select value={bestGift} onChange={handleBestGiftChange}>
+                  <option value="">Select a Gift</option>
+                  {gifts
+                    .filter((gift, index) => index < 65) // Limit to the first 65 items
+                    .map((gift) => (
+                      <option key={gift.id} value={gift.gift_name}>
+                        {gift.gift_name}
+                      </option>
+                    ))}
+                </select>
+                {bestGift && (
+                  <div className="gift-image-preview">
+                    <h4>Gift Image:</h4>
+                    <img
+                      src={bestGiftImage}
+                      alt="Selected Gift"
+                      className="gift-thumbnail"
+                    />
+                  </div>
+                )}
+              </label>
+              <button type="submit">Submit</button>
             </form>
           </div>
         </div>
